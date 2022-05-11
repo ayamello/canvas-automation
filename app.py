d3207c5da9da5fca6e199b6ba21f55ea608de14b
Ayana <ayana@kenzie.com.br>
2022-05-11 15:48:40 -0300

    feat: develop add_section

--- app.py
+++ app.py
@@ -28,12 +28,14 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 dotenv.load_dotenv()
 environs = dotenv.dotenv_values('.env')
 
-######################## Read wordsheet ########################## 
+######################## Read wordsheet ##########################
 table = pd.read_excel('./students.xlsx')
 
 list_emails = ', '.join(list(table['Email']))
@@ -45,12 +47,12 @@ navegador.get('https://alunos2.kenzie.com.br')
 
 time.sleep(int(environs['LOADING_TIME']))
 
-input_email = navegador.find_element(By.XPATH, 
-        '/html/body/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[2]/form[1]/div[1]/input')
+input_email = navegador.find_element(By.XPATH,
+                '/html/body/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[2]/form[1]/div[1]/input')
 input_email.send_keys(environs['EMAIL_CANVAS'])
 
-input_password = navegador.find_element(By.XPATH, 
-        '/html/body/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[2]/form[1]/div[2]/input')
+input_password = navegador.find_element(By.XPATH,
+                '/html/body/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[2]/form[1]/div[2]/input')
 input_password.send_keys(environs['PASSWORD_CANVAS'] + Keys.ENTER)
 
 module = navegador.find_element(By.XPATH, environs['MODULE_PATH'])
@@ -62,24 +64,55 @@ module.click()
 time.sleep(int(environs['LOADING_TIME']))
 
 link_peoples = navegador.find_element(By.XPATH,
-        '/html/body/div[2]/div[2]/div[2]/div[2]/nav/ul/li[6]/a')
+                '/html/body/div[2]/div[2]/div[2]/div[2]/nav/ul/li[6]/a')
 link_peoples.click()
 
 time.sleep(int(environs['LOADING_TIME']))
 
 if(environs['ACTION'] == 'add_student'):
     btn_add_student = navegador.find_element(By.XPATH,
-            '/html/body/div[2]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/a')
+                '/html/body/div[2]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/a')
     btn_add_student.click()
 
-    text_area = navegador.find_element(By.XPATH, 
-            '/html/body/span/span/span/div[2]/div/div/fieldset[2]/label/span/span[1]/span[2]/div/textarea')
+    text_area = navegador.find_element(By.XPATH,
+                '/html/body/span/span/span/div[2]/div/div/fieldset[2]/label/span/span[1]/span[2]/div/textarea')
     text_area.click()
     text_area.send_keys(list_emails)
 
     select_section = navegador.find_element(By.XPATH,
-            '//*[@id="peoplesearch_select_section"]')
+                '//*[@id="peoplesearch_select_section"]')
     select_section.click()
-    
+
+elif(environs['ACTION'] == 'add_section'):
+    array_emails = list_emails.split(', ')
+    for i in range(len(array_emails)):
+        field_email = navegador.find_element(By.XPATH, 
+                '/html/body/div[2]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/input')
+        field_email.send_keys(array_emails[i])
+
+        time.sleep(3)
+
+        three_dots = navegador.find_element(By.XPATH,
+                '/html/body/div[2]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/div/div[2]/table/tbody/tr/td[9]/div/a/i')
+        three_dots.click()
+
+        time.sleep(2)
+
+        btn_edit_section = navegador.find_element(By.XPATH, 
+                '/html/body/div[2]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/div/div[2]/table/tbody/tr/td[9]/div/ul/li[2]')
+        btn_edit_section.click()
+
+        time.sleep(3)
+
+        field_section = navegador.find_element(By.XPATH, 
+                '//*[@id="edit_sections"]/div/div/input')
+        field_section.send_keys(environs['FACILITADOR_SECTION'] + Keys.ENTER)
+        time.sleep(2)
+        field_section.send_keys(Keys.ENTER)
+
+        navegador.find_element(By.XPATH, 
+                '/html/body/div[3]/div[3]/div/button[1]').click()
+
+        field_email.clear()
 
 # navegador.quit()
